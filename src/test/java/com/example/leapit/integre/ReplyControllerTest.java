d4e57c0c179f18ba619442b7da7be59dca3a1136
parangdajavous <nuit0535@gmail.com>
2025-05-22 15:11:44 +0900

    [feat] 댓글 통합 테스트 (#53)
    댓글 통합테스트 완료 -> 오류 없음 확인
    
    api 문서는 기업정보 통합테스트 수정 후 test 예정

--- src/test/java/com/example/leapit/integre/ReplyControllerTest.java
+++ src/test/java/com/example/leapit/integre/ReplyControllerTest.java
@@ -1,4 +1,105 @@
 package com.example.leapit.integre;
 
+
+import com.example.leapit._core.util.JwtUtil;
+import com.example.leapit.board.reply.ReplyRequest;
+import com.example.leapit.common.enums.Role;
+import com.example.leapit.user.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.hamcrest.Matchers.matchesPattern;
+import static org.hamcrest.Matchers.nullValue;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class ReplyControllerTest {
+
+    @Autowired
+    private ObjectMapper om;
+
+    @Autowired
+    private MockMvc mvc;
+
+    private String accessToken;
+
+    @BeforeEach
+    public void setUp() {
+        // 테스트 시작 전에 실행할 코드
+        System.out.println("setUp");
+        User ssar = User.builder()
+                .id(1)
+                .username("ssar")
+                .role(Role.valueOf("PERSONAL"))
+                .build();
+        accessToken = JwtUtil.create(ssar);
+    }
+
+    // 댓글 삭제
+    @Test
+    public void save_test() throws Exception {
+        // given
+        ReplyRequest.SaveDTO reqDTO = new ReplyRequest.SaveDTO();
+        reqDTO.setBoardId(1);
+        reqDTO.setContent("댓글7");
+
+        String requestBody = om.writeValueAsString(reqDTO);
+        System.out.println(requestBody);
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .post("/s/api/personal/reply")
+                        .content(requestBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer " + accessToken)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.id").value(7));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.content").value("댓글7"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.name").value("쌀"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.boardId").value(1));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body.createdAt",
+                matchesPattern("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\.\\d{3}\\+\\d{2}:\\d{2}")));
+    }
+
+    @Test
+    public void delete_test() throws Exception {
+        // given
+        Integer id = 1;
+
+        // when
+        ResultActions actions = mvc.perform(
+                MockMvcRequestBuilders
+                        .delete("/s/api/personal/reply/{id}", id)
+                        .header("Authorization", "Bearer " + accessToken)
+        );
+
+        // eye
+        String responseBody = actions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+
+        // then
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(200));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("성공"));
+        actions.andExpect(MockMvcResultMatchers.jsonPath("$.body").value(nullValue()));
+    }
 }
